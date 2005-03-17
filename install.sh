3b5cee7ea92365d2739ab96ccbf37a358bdf1b84
gaetan.lehmann <gaetan.lehmann@jouy.inra.fr>
2005-03-17 20:06:28 +0100

    add remote authentication
    darcs-hash:20050317190628-ce5ba-c2d4111dd053f331198443efdba30c84b53920a4.gz

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ mkdir -p $DESTDIR/etc/rc.d/init.d
 mkdir -p $DESTDIR/etc/ncsmtp
 
 install -m 755 ncsmtpd  $DESTDIR/usr/sbin
-install -m 644 main.conf aliases $DESTDIR/etc/ncsmtp
+install -m 600 main.conf $DESTDIR/etc/ncsmtp
+install -m 644 aliases $DESTDIR/etc/ncsmtp
 install -m 755 ncsmtp.init.d $DESTDIR/etc/rc.d/init.d/ncsmtp
 
